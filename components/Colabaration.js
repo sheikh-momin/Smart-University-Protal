c8df5e0dd757459e6590e7c5d0b4ba6fbf9f6f59
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2023-01-13 11:03:42 +0600

    international colaboration hight set up

--- components/Colabaration.js
+++ components/Colabaration.js
@@ -5,14 +5,14 @@ const Colabaration = () => {
         <div>
             <p className='text-center mb-6 text-teal-700 text-3xl font-bold italic font-serif'>International collaboration</p>
             <div className='grid grid-cols-8 gap-4 mb-3  '>
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1331386988.jpg" />
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-academy-vector-icons-emblems-600w-1951925098.jpg" /> 
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253571742.jpg" />
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253589607.jpg" />
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1331386988.jpg" />
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-academy-vector-icons-emblems-600w-1951925098.jpg" /> 
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253571742.jpg" />
-            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253589607.jpg" />
+            <img className='border-4 h-40' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1331386988.jpg" />
+            <img className='border-4 h-40 ' src="https://www.shutterstock.com/image-vector/university-academy-vector-icons-emblems-600w-1951925098.jpg" /> 
+            <img className='border-4 h-40 ' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253571742.jpg" />
+            <img className='border-4 h-40' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253589607.jpg" />
+            <img className='border-4 h-40' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1331386988.jpg" />
+            <img className='border-4 h-40' src="https://www.shutterstock.com/image-vector/university-academy-vector-icons-emblems-600w-1951925098.jpg" /> 
+            <img className='border-4 h-40' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253571742.jpg" />
+            <img className='border-4 h-40' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253589607.jpg" />
         </div>
         </div>
         
