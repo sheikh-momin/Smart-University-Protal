411dc6f51c1bc8a8da48ece169676bd2b574b813
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2023-01-13 10:39:22 +0600

    international colaboration

---
+++ components/Colabaration.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Colabaration = () => {
+    return (
+        <div>
+            <p className='text-center mb-6 text-teal-700 text-3xl font-bold italic font-serif'>International collaboration</p>
+            <div className='grid grid-cols-8 gap-4 mb-3  '>
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1331386988.jpg" />
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-academy-vector-icons-emblems-600w-1951925098.jpg" /> 
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253571742.jpg" />
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253589607.jpg" />
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1331386988.jpg" />
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-academy-vector-icons-emblems-600w-1951925098.jpg" /> 
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253571742.jpg" />
+            <img className='border-4' src="https://www.shutterstock.com/image-vector/university-logo-template-600w-1253589607.jpg" />
+        </div>
+        </div>
+        
+    );
+};
+
+export default Colabaration;
--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carusol from "../../components/Carusol";
+import Colabaration from "../../components/Colabaration";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import Faculty from "../faculty";
@@ -9,6 +10,7 @@ const HomePage = () => {
       <Navbar></Navbar>
       <Carusol></Carusol>
       <Faculty></Faculty>
+      <Colabaration></Colabaration>
       <Footer></Footer>
     </div>
   );
