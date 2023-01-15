e6c1708d374bcf822db5e72f12ebe6e309adb11a
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2023-01-15 22:16:37 +0600

    campus life hostel facilities

--- pages/campuslife/index.js
+++ pages/campuslife/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Facilities from '../../components/campusLife/Facilities';
+import Hostel from '../../components/campusLife/Hostel';
 import StudentActivities from '../../components/campusLife/StudentActivities';
 import Navbar from '../../components/Navbar';
 
@@ -9,6 +10,7 @@ const Campuslife = () => {
             <Navbar></Navbar>
             <StudentActivities></StudentActivities>
             <Facilities></Facilities>
+            <Hostel></Hostel>
         </div>
     );
 };
