b4463e4e6957becbe64428500a099c6614d21d15
ShahanazAhmedNishi <asahanaz207@gmail.com>
2023-01-20 03:16:45 +0600

    chat

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import HomePage from "./home";
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 const inter = Inter({ subsets: ["latin"] });
 
