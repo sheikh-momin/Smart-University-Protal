d64c13c684f0a52dc10da7d0fdf1a4270d0a0af0
sheikh-momin <momin.emon14@gmail.com>
2023-01-26 22:21:21 -0800

    merge

--- pages/dashboard/dashboardNav/Dashboardnav.js
+++ pages/dashboard/dashboardNav/Dashboardnav.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { AuthContext } from '../../../Context/AuthProvider';
 
 const Dashboardnav = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleSingOut = () => {
+        logOut()
+            .then(() => { })
+            .catch(error => console.error(error))
+
+    }
     return (
 
          <div className="border-slate-100 border-r-2 mt-10 bg-teal-800">
@@ -82,7 +91,7 @@ const Dashboardnav = () => {
                 <li>
                     <a href="#" class="flex items-center p-2 text-base font-normal text-white rounded-lg dark:text-black hover:bg-black dark:hover:bg-gray-700">
                     <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6 text-white transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                        <span class="flex-1 ml-3 whitespace-nowrap font-bold">log Out</span>
+                        <span onClick={handleSingOut} class="flex-1 ml-3 whitespace-nowrap font-bold">log Out</span>
                     </a>
                 </li>
             </ul>
