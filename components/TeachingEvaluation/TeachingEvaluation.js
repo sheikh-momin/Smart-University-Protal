77d33a605eacd917fdeea550f4fd91b064afabe2
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2023-01-31 12:36:51 +0600

    tea eva

--- components/TeachingEvaluation/TeachingEvaluation.js
+++ components/TeachingEvaluation/TeachingEvaluation.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 import React from 'react';
 import Dashboardnav from '../../pages/dashboard/dashboardNav/Dashboardnav';
 import DashboardNavbar from '../../pages/dashboard/dashboardNav/DashboardNavbar';
@@ -7,10 +7,67 @@ const TeachingEvaluation = () => {
     return (
         <div>
         <DashboardNavbar></DashboardNavbar>
-         <div className='flex bg-teal-800'>
+         <div className='flex bg-teal-600'>
             {/* <DashboardNavbar */}
 
             <Dashboardnav></Dashboardnav>
+            <div className="form-control text-left w-full max-w-3xl mt-10 ml-40">
+                <label className="label text-center text-slate-800 bg-teal-500">
+                    <span className=" text-center text-3xl">Dreamers University Teaching Evaluation</span>
+                </label>
+                <select className="mt-5  bg-teal-200 select select-success w-full max-w-xs">
+                <option disabled selected>Select Semester</option>
+                <option>Fall-2021</option>
+                <option>Summer-2021</option>
+                <option>Spring-2022</option>
+                <option>Fall-2022</option>
+                <option>Summer-2022</option>
+                <option>Spring-2023</option>
+                <option>Fall-2023</option>
+                </select>  
+
+                <div>
+                    <table class="mt-10 w-full border-collapse border border-slate-300 ...">
+                    <thead>
+                        <tr class=" text-2xl border bg-info border-slate-300 ...">
+                        <th class="">Submit Status</th>
+                        <th class="">Course Title</th>
+                        <th class="">Course Name</th>
+                        <th class="">Teacher Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class=" text-xl border bg-secondary-focus border-slate-300 ...">
+                        <td class="">Indiana</td>
+                        <td class="">Indianapolis</td>
+                        <td class="">Indianapolis</td>
+                        <td class="">Indianapolis</td>
+                        </tr>
+                        <tr class="text-xl border bg-primary-focus border-slate-300 ...">
+                        <td class="">Ohio</td>
+                        <td class="">Columbus</td>
+                        <td class="">Columbus</td>
+                        <td class="">Columbus</td>
+                        </tr>
+                        <tr class="text-xl border bg-success border-slate-300 ...">
+                        <td class="">Michigan</td>
+                        <td class="">Detroit</td>
+                        <td class="">Detroit</td>
+                        <td class="">Detroit</td>
+                        </tr>
+                        <tr class=" text-xl border bg-warning border-slate-300 ...">
+                        <td class="">Michigan</td>
+                        <td class="">Detroit</td>
+                        <td class="">Detroit</td>
+                        <td class="">Detroit</td>
+                        </tr>
+                    </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+            
 
             {/* <div className='mt-10 ml-16'>
                 <h2 className='text-2xl font-bold text-white text-center'>Faculty of Engineering</h2>
