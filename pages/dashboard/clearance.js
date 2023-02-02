13a4520bf608bf30e8e705f6e56c2fa85b01cca3
sheikh-momin <momin.emon14@gmail.com>
2023-02-02 02:22:11 -0800

    Registered Course, clearance

--- pages/dashboard/clearance.js
+++ pages/dashboard/clearance.js
@@ -6,6 +6,7 @@ import DashboardNavbar from "./dashboardNav/DashboardNavbar";
 const Clearance = () => {
   const { user } = useContext(AuthContext)
   const [clearance, setClearance] = useState([])
+  
   useEffect(() => {
     if (user?.email) {
       fetch(`http://localhost:5000/clearance/${user?.email}`)
@@ -14,31 +15,31 @@ const Clearance = () => {
           setClearance(data)
         })
     }
-  }, [user])
+  },[])
   
   return (
-    <div>
+    <div className="bg-[#e4e4e7] pb-80">
       <DashboardNavbar></DashboardNavbar>
       <div className="bg-slate-200 drop-shadow-lg py-3 mt-3">
-        <h1 className="text-center text-cyan-500 text-3xl font-bold italic font-serif">Registration/Exam Clearance</h1>
+        <h1 className="text-center text-cyan-500 md:text-3xl text-lg font-bold italic font-serif">Registration/Exam Clearance</h1>
       </div>
 
-      <div className="overflow-x-auto mt-5 md:mx-40">
+      <div className="overflow-x-auto mt-20 md:mx-40">
         <table className="table w-full">
 
           <thead className="">
-            <tr>
-              <th>
+            <tr >
+              <th className="bg-cyan-500">
                 Semester</th>
-              <th>Registration</th>
-              <th>Mid Term Exam</th>
-              <th>Final Exam/Assessment</th>
+              <th className="bg-cyan-500">Registration</th>
+              <th className="bg-cyan-500">Mid Term Exam</th>
+              <th className="bg-cyan-500">Final Exam/Assessment</th>
             </tr>
           </thead>
           <tbody>
           
           {
-              clearance.map(cleat => <tr>
+              clearance.map(cleat => <tr key={cleat._id}>
                 <th>{cleat.semester}</th>
                 <td>{cleat.registration}</td>
                 <td>{cleat.midExm}</td>
--- pages/dashboard/registeredCourse.js
+++ pages/dashboard/registeredCourse.js
@@ -6,22 +6,27 @@ import DashboardNavbar from "./dashboardNav/DashboardNavbar";
 
 const RegisteredCourse = () => {
   const { user } = useContext(AuthContext)
-  const [courses, setCourses] = useState()
-  const [semester, setSemester] = useState()
+  const [courses, setCourses] = useState([ ])
+  const [semester, setSemester] = useState("")
+  const [loading , setLoading] = useState(true)
   const handleSemester =(e)=>{
     setSemester(e.target.value)
   }
 
   useEffect(() => {
     if (user?.email) {
-      fetch(`http://localhost:5000/registeredCourseList/${user?.email}`)
+      fetch(`http://localhost:5000/registeredCourseList/${semester}`)
         .then(res => res.json())
-        .then(data => setCourses(data))
+        .then(data => {
+          setCourses(data.subject)
+          setLoading(false)
+        })
     }
-  }, [user])
-
+  },[])
+  console.log("courses",courses)
+  console.log(semester);
   return (
-    <div>
+    <div className="bg-[#e4e4e7] pb-80">
       <DashboardNavbar></DashboardNavbar>
       <div className="bg-slate-200 drop-shadow-lg py-3 mt-3">
         <h1 className="text-center text-cyan-500 text-3xl font-bold italic font-serif">Registered Course</h1>
@@ -31,10 +36,10 @@ const RegisteredCourse = () => {
         <label className="label mr-1">
           <span className="label-text">Select Semester</span>
         </label>
-        <select onClick={handleSemester} name="semester" className="select select-bordered w-full max-w-xs">
-          <option>Spring-2023</option>
-          <option>Summer-2023</option>
-          <option>Fall-2023</option>
+        <select onChange={handleSemester} name="semester" className="select select-bordered w-full max-w-xs">
+          <option selected value="Spring2023">Spring2023</option>
+          <option>Summer2023</option>
+          <option>Fall2023</option>
         </select>
 
       </div>
@@ -61,20 +66,20 @@ const RegisteredCourse = () => {
             <tbody>
 
               {
-                courses?.data?.map(course=>{
-                  console.log(course.subject)
-                })
+                courses?.map((course)=>
+                  <tr key={course.CourseCode}>
+                  <th>{course.CourseCode}</th>
+                  <th>{course.CourseTitle}</th>
+                    <th >{course.credit}</th>
+                    <th>{course.section}</th>
+                    <th>{course.teacher}</th>
+                    <th>{course.advise}</th>
+                    <th>{course.clearance}</th>
+                </tr>
+                )
               }
 
-              <tr>
-                <th>1</th>
-                <td>Brice Swyre</td>
-                <td>Tax Accountant</td>
-                <td>Red green</td>
-                <td>Red</td>
-                <td>Red</td>
-                <td>Red</td>
-              </tr>
+              
             </tbody>
           </table>
         </div>
