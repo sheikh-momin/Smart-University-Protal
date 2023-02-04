2bfe8066098a75b0f7590bd01a59498003826b20
Obaidul hasan <md.obaidulhasnlimu@gmaiol.comgit config --global user.name Obaidul>
2023-02-04 17:55:03 +0600

    Page Redesign

--- components/RoutineAndNotics/RoutineAndNotics.js
+++ components/RoutineAndNotics/RoutineAndNotics.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import Drawer from "../../components/Dashboard/Drawer";
 import Dashboardnav from '../../pages/dashboard/dashboardNav/Dashboardnav';
 import DashboardNavbar from '../../pages/dashboard/dashboardNav/DashboardNavbar';
 
 const RoutineAndNotics = () => {
     return (
-        <div>
+        <div className="bg-[#d4d4d8] pb-80">
         <DashboardNavbar></DashboardNavbar>
+        <div className="bg-slate-200 drop-shadow-lg py-3 ">
+        <h1 className="text-center text-cyan-500 text-3xl font-bold italic font-serif">Class Routine And Notices</h1>
+        </div>
          <div className='flex bg-zinc-300'>
             {/* <DashboardNavbar */}
 
-            <Dashboardnav></Dashboardnav>
+            <div>
+             <Drawer></Drawer>
+            </div>
 
-            <div className="form-control text-center w-full max-w-3xl mt-10 ml-40">
-                <label className="label text-center text-white bg-teal-400">
+            <div className="form-control text-center w-full max-w-3xl mt-10 ml-20">
+                {/* <label className="label text-center text-white bg-teal-400">
                     <span className=" text-center text-3xl">Class Routine</span>
-                </label>
+                </label> */}
 
-                <div className=" flex form-control  w-full max-w-4xl mt-10  bg-teal-300 ">
+                <div className=" flex form-control  w-full max-w-4xl mt-7 ml-10 bg-cyan-300 ">
                     <div>
                         <div className="form-control w-full max-w-2xl mt-2 ml-10 grid grid-cols-1 gap-5">        
                             <div>
@@ -27,7 +33,7 @@ const RoutineAndNotics = () => {
                     </div>                     
                 </div>
 
-                <div className=" flex form-control  w-full max-w-3xl mt-5  ">
+                <div className=" flex form-control  w-full max-w-3xl mt-5 ml-10 ">
                     <div>
                         <div className="form-control w-full max-w-3xl  ml-10 grid grid-cols-2 gap-5">                       
                             <div>
@@ -35,7 +41,7 @@ const RoutineAndNotics = () => {
                                     <label className="label">
                                         <span className="label-text bg-base-200 text-teal-900 text-lg">Select Semester</span>
                                     </label>
-                                    <select className="select select-bordered  bg-teal-200  select-success w-full max-w-3xl">
+                                    <select className="select select-bordered  bg-cyan-200 select-success w-full max-w-3xl">
                                         <option disabled selected>Click Here</option>
                                         <option>Spring-2021</option>
                                         <option>Fall-2022</option>
@@ -54,77 +60,77 @@ const RoutineAndNotics = () => {
                 </div>
 
                 
-                <div className=" flex form-control  w-full max-w-3xl mt-10  bg-white ">
+                <div className=" flex form-control  w-full max-w-3xl mt-10 ml-10  bg-white ">
                     <div>
                         <div className="form-control w-full max-w-2xl mt-2 ml-5 grid grid-cols-1 gap-5">   
-                            <h1 className='text-center text-slate-600 bg-teal-100 text-xl ml-10 mt-5'>Academic Class Routine of :</h1>
+                            <h1 className='text-center text-slate-600 bg-cyan-200 text-xl ml-10 mt-5'>Academic Class Routine of :</h1>
 
                             <div className="collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Saturday
                                 </div>
-                                <div className="collapse-content bg-teal-500 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-300 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>
 
                             <div className="collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Sunday
                                 </div>
-                                <div className="collapse-content bg-teal-400 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-3000 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>
 
                             <div className="collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Monday
                                 </div>
-                                <div className="collapse-content bg-teal-400 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-300 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>
 
                             <div className="collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Tuesday
                                 </div>
-                                <div className="collapse-content bg-teal-400 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-300 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>
 
                             <div className="collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Wednesday
                                 </div>
-                                <div className="collapse-content bg-teal-400 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-300 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>
 
                             <div className="collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Thursday
                                 </div>
-                                <div className="collapse-content bg-teal-400 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-300 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>
 
                             <div className=" md-1 collapse ml-10">
                                 <input type="checkbox" className="peer" /> 
-                                <div className="collapse-title bg-teal-300 text-primary-content peer-checked:bg-teal-400 peer-checked:text-secondary-content">
+                                <div className="collapse-title bg-cyan-300 text-primary-content peer-checked:bg-cyan-300 peer-checked:text-secondary-content">
                                     Friday
                                 </div>
-                                <div className="collapse-content bg-teal-400 text-primary-content peer-checked:bg-teal-100 peer-checked:text-secondary-content"> 
+                                <div className="collapse-content bg-cyan-300 text-primary-content peer-checked:bg-cyan-100 peer-checked:text-secondary-content"> 
                                     <p>hello</p>
                                 </div>
                             </div>                                                       
@@ -137,7 +143,7 @@ const RoutineAndNotics = () => {
                 </div>
 
 
-                <div className=" flex form-control  w-full max-w-4xl mt-5  bg-teal-400 ">
+                <div className=" flex form-control  w-full max-w-4xl mt-5 ml-10 bg-cyan-400 ">
                     <div>
                         <div className="form-control w-full max-w-4xl mt-2  grid grid-cols-1 gap-5">        
                             <div>
@@ -153,9 +159,9 @@ const RoutineAndNotics = () => {
                             <div>
                                 <div className="form-control w-full max-w-3xl">
                                     <label className="label">
-                                        <span className="label-text bg-base-200 text-teal-900 text-lg">Program</span>
+                                        <span className="label-text bg-cyan-100 text-teal-900 text-lg">Program</span>
                                     </label>
-                                    <select className="select select-bordered  bg-teal-200  select-success w-full max-w-3xl">
+                                    <select className="select select-bordered  bg-cyan-200  select-success w-full max-w-3xl">
                                         <option disabled selected>Pick a Program</option>
                                         <option>Computer Science and Engineering</option>
                                         <option>Business Administration</option>
