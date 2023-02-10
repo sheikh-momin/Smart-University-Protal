331b89b5242a399ee9de51dc4ce4016c9515154a
sheikh-momin <momin.emon14@gmail.com>
2023-02-10 06:08:47 -0800

    pulling

--- components/Dashboard/Dashboard.js
+++ components/Dashboard/Dashboard.js
@@ -13,7 +13,7 @@ const Dashboard = () => {
         </div>
 
         <div className="grid grid-cols-3 gap-4 md:w-9/12 md:mx-auto mx-5 text-white mt-10">
-          <div className="bg-amber-300 md:p-8 p-2 rounded-xl">
+          <div className="bg-amber-300 md:p-8  p-2 rounded-xl">
             <h4 className="md:text-2xl">Total Payable</h4>
             <h2 className="md:text-5xl">$14000</h2>
           </div>
@@ -21,7 +21,7 @@ const Dashboard = () => {
             <h4 className="md:text-2xl">Total Paid</h4>
             <h2 className="md:text-5xl">$7000</h2>
           </div>
-          <div className="bg-blue-400 md:p-8 p-2 rounded-xl">
+          <div className="bg-blue-400  md:p-8 p-2 rounded-xl">
             <h4 className="md:text-2xl">Total Due</h4>
             <h2 className="md:text-5xl">$7000</h2>
           </div>
